119fdd7d024eca5617bbe626719e28796934241d
noob <noobie37@protonmail.com>
2021-06-16 22:37:08 +0530

    Updates

---
+++ precache-manifest.8502b1beb9907bde8fc59abd3b708eb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d221d1c98b24e54ad824743382d9192",
+    "url": "/contact-data/index.html"
+  },
+  {
+    "revision": "b9a0dbad9ccff22bc6c1",
+    "url": "/contact-data/static/js/2.9829a888.chunk.js"
+  },
+  {
+    "revision": "1a4263a84b07f74bfa91",
+    "url": "/contact-data/static/js/main.61d811e0.chunk.js"
+  },
+  {
+    "revision": "5f3f70efe7eb07fa280a",
+    "url": "/contact-data/static/js/runtime-main.e1c3d458.js"
+  }
+]);
